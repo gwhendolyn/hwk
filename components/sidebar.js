edaffbc16046d15470cfd74f8a144da78e243af7
Gwen <bones2406@gmail.com>
2024-11-13 16:33:24 -0700

    filling sidebar buttons

--- components/sidebar.js
+++ components/sidebar.js
@@ -3,13 +3,13 @@ export default function Sidebar(props){
     
     return(
         <div className={styles.container} id={props.lm ? null:"inverted"}>
-            <button className={styles.button}>
+            <button className={styles.button} onClick={props.toggleSave}>
                 <img className={styles.icon} src="/hwk/save.svg"/>
             </button>
 
             <hr className={styles.rule}/>
 
-            <button className={styles.button}>
+            <button className={styles.button} onClick={props.toggleLoad}>
                 <img className={styles.icon} src="/hwk/open.svg"/>
             </button>
 
@@ -21,7 +21,7 @@ export default function Sidebar(props){
 
             <hr className={styles.rule}/>
 
-            <button className={styles.button}>
+            <button className={styles.button} onClick={props.toggleInfo}>
                 <img className={styles.icon} src="/hwk/info.svg"/>
             </button>
 
--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,54 @@ import styles from '../styles/Home.module.css';
 import Sidebar from '../components/Sidebar.js';
 import CourseContainer from '../components/CourseContainer.js';
 import React, { useState } from 'react';
-
 export default function Home() {
   const [lightMode, setLightMode] = useState(true);
+  const [load, setLoad] = useState(false);
+  const [save, setSave] = useState(false);
+  const [info, setInfo] = useState(false);
+  const [CCID, setCCID] = useState(0);
   const invertLightMode = () =>{
     setLightMode(!lightMode);
   }
+  const toggleLoad = () =>{
+    setLoad(!load);
+  }
+  const toggleSave = () =>{
+    setSave(!save);
+  }
+  const toggleInfo = () =>{
+    setInfo(!info);
+  }
+
+  
   return (
     <div className={styles.container}>
+      {/* --loading card-- */}
+      {/* {load? (<div className={styles.cardbg}>
+
+      </div>):(null)} */}
+      
+      {/* --saving card-- */}
+      {/* {save? (<div className={styles.cardbg}>
+
+      </div>):(null)} */}
+      
+      {/* --info card-- */}
+      {/* {info? (<div className={styles.cardbg}>
+
+      </div>):(null)} */}
+      
+      {/* main content */}
       <Head>
         <title>HWK: Grade Calculator</title>
         <link rel="icon" href="/hwk/favicon.ico" />
       </Head>
-      <Sidebar lm={lightMode} setLm={invertLightMode}/>
-      <CourseContainer lm={lightMode}/>
+      <Sidebar lm={lightMode} 
+      setLm={invertLightMode} 
+      toggleLoad={toggleLoad} 
+      toggleSave={toggleSave}
+      toggleInfo={toggleInfo}/>
+      <CourseContainer key={CCID} lm={lightMode}/>
     </div>
   );
 }
